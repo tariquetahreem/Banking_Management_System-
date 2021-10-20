fb692980319f7d730b98ec1bf2cb901efb1d0a9b
Tahreem Tarique <91584173+tariquetahreem@users.noreply.github.com>
2021-10-20 10:17:15 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!-- navbar --> 
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : hsl(120, 100%, 25%);"><b>SPARKS BANK OF INDIA</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : hsl(120, 100%, 25%);"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : hsl(120, 100%, 25%);"><b>Create User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : hsl(120, 100%, 25%);"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : hsl(120, 100%, 25%);"><b>Transaction Details</b></a>
+              </li>
+          </div>
+       </nav>
---
+++ tsf_task1.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 10, 2021 at 05:55 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tsf_task1.sql`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `sno` int(3) NOT NULL,
+  `sender` text NOT NULL,
+  `receiver` text NOT NULL,
+  `balance` int(8) NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`sno`, `sender`, `receiver`, `balance`, `datetime`) VALUES
+(1, 'Tahreem', 'Tanno', 50000, '2021-10-18 22:46:18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(3) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `balance` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
+(1, 'Tahreem', 'tahreem70@gmail.com', 480000),
+(2, 'Tanno', 'tanno3@gmail.com', 700000),
+(3, 'Tarique', 'tarique4@gmail.com', 1550000),
+(4, 'Faleha', 'faleha@gmail.com', 370000),
+(5, 'Reem', 'reem@gmail.com', 470000),
+(6, 'Amayra', 'amayra@gmail.com', 580000),
+(7, 'Aman', 'aman@gmail.com', 750000),
+(8, 'Abiha', 'abiha5@gmail.com', 820000),
+(9, 'Kashif', 'kashif@gmail.com', 660000),
+(10, 'Amritanjali', 'amritanjali@gmail.com', 770000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`sno`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `sno` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
